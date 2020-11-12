351e8502f4154265270ba9c2fb41bbc9fa550eed
Taylor <tjaroguetech@gmail.com>
2020-11-12 10:15:47 -0500

    commit tree 028b08fbb721231b4ad2c84dcd3ff1910ae8be46

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(args) {
+    for (let i = 0; i < args.length; i++) {
+        args[i] = `Thank you, ${args[i]}, for the wonderful surprise gift!`;
+    }
+
+    return args;
+}
+
+function countDown(num) {
+    while (num > 0) {
+        console.log(num)
+        num--
+    }
+    console.log( num );
+}
